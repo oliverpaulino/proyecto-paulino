664109c83b2a0b75550e5731b8f04e3959cdb0e3
Samuel Olivo <olivopichardosamuel@gmail.com>
2026-07-11 03:28:23 -0400

    CON-134: Restaurar OC

--- src/stores/usePurchaseOrderStore.ts
+++ src/stores/usePurchaseOrderStore.ts
@@ -27,6 +27,7 @@ type PurchaseOrderStore = {
       estado: EstadoOrdenCompra
    ) => Promise<void | Error>;
    DeletePurchaseOrder: (id: string) => Promise<void | Error>;
+   RestorePurchaseOrder: (id: string) => Promise<void | Error>;
    CheckIsApprover: () => Promise<boolean>;
    invalidateCache: () => void;
 };
@@ -97,14 +98,11 @@ export const usePurchaseOrderStore = create<PurchaseOrderStore>((set, get) => ({
             body: JSON.stringify(form),
          });
 
-         // 1. PRIMERO validamos si falló
          if (!res.ok) {
-            // Leemos como texto plano para evitar el SyntaxError si es un 404 o error del servidor
             const errorText = await res.text();
             throw new Error(`Error ${res.status}: ${errorText}`);
          }
 
-         // 2. LUEGO parseamos el JSON (porque sabemos que res.ok es true)
          const data = await res.json();
 
          get().invalidateCache();
@@ -188,6 +186,27 @@ export const usePurchaseOrderStore = create<PurchaseOrderStore>((set, get) => ({
 
          get().invalidateCache();
          await get().GetPurchaseOrders();
+      } catch (error) {
+         return error as Error;
+      }
+   },
+
+   RestorePurchaseOrder: async (id) => {
+      try {
+         const res = await fetch(`/api/purchase-orders/${id}/restore`, {
+            method: "PATCH",
+         });
+
+         const responseData = await res.json();
+         if (!res.ok)
+            throw new Error(
+               responseData.error ||
+               responseData.message ||
+               "Error al restaurar orden de compra"
+            );
+
+         get().invalidateCache();
+         await get().GetPurchaseOrdersDeleted({ force: true });
       } catch (error) {
          return error as Error;
       }
