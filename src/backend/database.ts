4c1f3a5391fcd8927c32af8eda1aaca80da61a4e
Samuel Olivo <olivopichardosamuel@gmail.com>
2026-07-16 00:49:34 -0400

    add categoria_gasto and gasto interfaces to DB schema

--- src/backend/database.ts
+++ src/backend/database.ts
@@ -391,6 +391,33 @@ export interface DB {
    servicio_tarifas: ServicioTarifaTable;
    proyecto_tarifas: ProyectoTarifaTable;
    proyecto_equipos: ProyectoEquipoTable;
+
+   categoria_gasto: {
+      id: Generated<string>;
+      nombre: string;
+      grupo: string;
+      created_at: Generated<Date>;
+      updated_at: Generated<Date>;
+   }
+   
+   gasto: {
+      id: Generated<string>;
+      referencia: Generated<number>;
+      monto_total: number;
+      concepto: string;
+      ncf: string;
+      entidad_responsable: string;
+      categoria_gasto_id: string;
+      orden_compra_id: string | null;
+      proyecto_id: string | null;
+      equipo_id: string | null;
+      empleado_id: string | null;
+      created_at: Generated<Date>;
+      updated_at: Generated<Date>;
+      deleted_by: string | null;
+      deleted_at: Date | null;
+      deleted_reason: string | null;
+   };
 }
 
 const db = new Kysely<DB>({
