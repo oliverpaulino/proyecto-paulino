995ce3b2e29ef84ea895a948b11d304fea9930ea
oliverjpaulinop <oliverjpaulinop@gmail.com>
2026-07-17 18:33:07 -0400

    Agregando transformador a fecha de la republica dominicana

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,3 +16,26 @@ export function catchError<T>(
       return [error];
     });
 }
+
+export const fechaRD = (value: string) => {
+
+  if (value === "day") {
+    return new Date().toLocaleString("es-DO", {
+      timeZone: "America/Santo_Domingo",
+      day: "2-digit",
+    });
+  } else if (value === "month") {
+    return new Date().toLocaleString("es-DO", {
+      timeZone: "America/Santo_Domingo",
+      month: "2-digit",
+    });
+  } else if (value === "year") {
+    return new Date().toLocaleString("es-DO", {
+      timeZone: "America/Santo_Domingo",
+      year: "numeric",
+    });
+  } else
+    return new Date().toLocaleString("es-DO", {
+      timeZone: "America/Santo_Domingo",
+    })
+};
